a57fe7df563f1e555ed60353432883ddef0f3e06
Matt Snell <msnell@unomaha.edu>
2018-08-27 22:04:23 -0500

    Linter and README fixes

--- src/background.js
+++ src/background.js
@@ -1,50 +1,50 @@
 "use strict";
-var epoch = (new Date).getTime();
+const epoch = (new Date).getTime();
 const cookies = [
     {
-		// NO-CONSENT
+        // NO-CONSENT
         // Working 8/24/2018 (M)
         domain: "mediapart.fr",
         name: "cc",
         value: "{%22disagreement%22:[%22visit%22%2C%22ad%22]%2C%22creation%22:" + epoch + "%2C%22update%22:" + epoch + "}",
     },
     {
-		//NO CONSENT
+        // NO CONSENT
         // Working 8/24/2018 (M)
         domain: "theguardian.com",
         name: "GU_TK",
         value: "0",
     },
-	{
-		//NO CONSENT
+    {
+        // NO CONSENT
         // Working 8/24/2018 (M)
         domain: "theguardian.co.uk",
         name: "GU_TK",
         value: "0",
     },
     {
-	    // NO CONSENT
+        // NO CONSENT
         // Working 8/24/2018 (M)
         domain: "www.independent.co.uk",
         name: "euconsent",
         value: "BOSl-jdOTLWH-AKAHCENBhAAAAAgkOy_"
-		+ "93f9_X1fvzf-_f_9__9_59__v____f3______"
-		+ "__-9________v7__v_9__________________"
-		+ "_________A",
+        + "93f9_X1fvzf-_f_9__9_59__v____f3______"
+        + "__-9________v7__v_9__________________"
+        + "_________A",
     },
     {
-		// NO CONSENT
+        // NO CONSENT
         // Working 8/24/2018 (M)
         domain: "www.thelocal.es",
         name: "euconsent",
         value: "BOTLWroOTLWroABADAENBhAAAAAgl7___"
-		+ "____9______9uz_Gv_v_f__33e8__9v_l_7_-_"
-		+ "__u_-33d4-_1vX99yfm1-7ftr3tp_86ues2_Xu"
-		+ "r_9pd3s",
+        + "____9______9uz_Gv_v_f__33e8__9v_l_7_-_"
+        + "__u_-33d4-_1vX99yfm1-7ftr3tp_86ues2_Xu"
+        + "r_9pd3s",
     },
     {
         // NO CONSENT
-		// Working 8/24/2018 (M)
+        // Working 8/24/2018 (M)
         domain: "www.telegraph.co.uk",
         name: "_evidon_consent_cookie",
         value: "{\"vendors\":{\"6\":[]},\"consent_" +
@@ -65,78 +65,78 @@ const css = [
         domain: "forsal.pl",
         selector: "#inforcwp",
     },
-	{
+    {
         domain: "index.hr",
         selector: "#cookie-consent-container",
     },
-	{
+    {
         domain: "investing.com",
         selector: "#consentBarWrapper",
     },
-	{
+    {
         domain: "thejournal.ie",
         selector: "#notify-container",
     },
-	{
+    {
         domain: "politico.eu",
-        selector: "#super-inner", 
-		//somehow becomes consent dialog
+        selector: "#super-inner",
+        // somehow becomes consent dialog
     },
-	{
-		//takes a little while to load
+    {
+        // takes a little while to load
         domain: "voterspost.com",
         selector: "#cookie-notice-container",
     },
-	{
+    {
         domain: "bbc.com",
         selector: "#bbccookies-banner",
     },
-	{
-		domain:"euractiv.com",
-		selector: "cookie-law-bar-transition-bottom",
-	},
-	{
-		//uses primary-key identities, couldnt get it to work quickly
-		domain:"nytimes.com",
-		selector: "#shown expanded css-sbgq86 e1i2wxpm0",
-	},
-	{
+    {
+        domain: "euractiv.com",
+        selector: "cookie-law-bar-transition-bottom",
+    },
+    {
+        // uses primary-key identities, couldnt get it to work quickly
+        domain: "nytimes.com",
+        selector: "#shown expanded css-sbgq86 e1i2wxpm0",
+    },
+    {
         domain: "ctxt.es",
         selector: "#aviso-cookies",
     },
-	{
+    {
         domain: "lepetitjournal.com",
         selector: "#popup-content",
     },
-	{
+    {
         domain: "portfolio.hu",
         selector: "#_iph_cp_standalone",
     },
-	{
+    {
         domain: "nouvelobs.com",
         selector: "#ObsCnil",
     },
-	{
+    {
         domain: "index.hu",
         selector: "#_iph_cp_popup",
     },
-	{
+    {
         domain: "reuters.com",
         selector: "#_evidon_banner",
     },
-	{
+    {
         domain: "shetnews.co.uk",
         selector: "#alert",
     },
-	{
+    {
         domain: "www.tgcom24.mediaset.it",
         selector: "#cookieGdpr",
     },
-	{
+    {
         domain: "www.tgcom24.mediaset.it",
         selector: "#cookieAdv",
     },
-	{
+    {
         domain: "trtworld.com",
         selector: "#gdpr-banner",
     },
@@ -162,7 +162,7 @@ let actions = {
                 css: [{code: `${c.selector} { display: none !important }`}],
                 runAt: "document_start",
             });
-			console.log(c.selector + " rule set for " + c.domain)
+            console.log(c.selector + " rule set for " + c.domain);
             contentScripts.push(cs);
         }
         browser.storage.sync.set({enabled: true});
@@ -180,7 +180,7 @@ let actions = {
             await cs.unregister();
         }
         contentScripts = [];
-        console.log("Cookies and CSS hiders disabled")
+        console.log("Cookies and CSS hiders disabled");
         browser.storage.sync.set({enabled: false});
     },
 };
