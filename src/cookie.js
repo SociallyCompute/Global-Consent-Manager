f6857aa42da84032f782e558535e1a240c71ff53
Matt Snell <msnell@unomaha.edu>
2018-09-07 16:15:52 -0500

    Fixed inconsistency with trust button

--- src/cookie.js
+++ src/cookie.js
@@ -17,7 +17,6 @@ let actions = {
         }
     },
 };
-
 async function handleMessage(message) {
     if (message.greet == "notList" && !message.stat) {
         document.getElementById("trust").innerHTML = "Website is not on the list.<br>(Reports coming soon)";
@@ -45,9 +44,19 @@ function check() {
 
 async function main() {
     let block = document.querySelector("#block");
-    let {enabled} = await browser.storage.sync.get();
+    let {enabled} = await browser.storage.sync.get("enabled");
     block.checked = enabled;
 
+    let {trusted} = await browser.storage.sync.get("trusted");
+    if (!trusted) {
+        document.getElementById("trust").innerHTML = "Website Not Trusted<br>(Click to Change)";
+        document.getElementById("checkTrust").checked = true;
+    } else if (trusted) {
+        document.getElementById("trust").innerHTML = "Website Trusted<br>(Click to Change)";
+        document.getElementById("checkTrust").checked = false;
+    }
+    console.log(enabled);
+    console.log(trusted);
     document.addEventListener("click", async (e) => {
         if (e.target.id == "block" || "trust") {
             await actions[e.target.id]();
