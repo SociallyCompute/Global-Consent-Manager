02758393d120234dc7d6ea11327ef279f7f6106b
Matt Snell <msnell@unomaha.edu>
2018-09-21 15:42:24 -0500

    Add more content to site, remove innerHtml

--- src/act.js
+++ src/act.js
@@ -1,11 +1,20 @@
 "use strict";
 
 async function sendSite() {
+    let contain = document.getElementById("container");
+    let children = contain.childNodes;
+    for (let i = 0; i < children.length; i++) {
+        contain.removeChild(children[i]);
+    }
+    let br;
+    let para = document.getElementById("container");
     let newContent = "";
     for (let i = 0; i < sites.length; i++) {
-        newContent += sites[i].domain + "<br>";
+        newContent = document.createTextNode(sites[i].domain);
+        br = document.createElement("br");
+        para.appendChild(br);
+        para.appendChild(newContent);
     }
-    document.getElementById("container").innerHTML = newContent;
 }
 
 window.addEventListener("DOMContentLoaded", sendSite);
