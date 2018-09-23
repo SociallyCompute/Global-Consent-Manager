882c52539ba1427b2cd8277d2f0bc2f02ae88b21
Matt Snell <msnell@unomaha.edu>
2018-09-22 19:44:49 -0500

    Add GitHub reporting and main text

--- src/act.js
+++ src/act.js
@@ -17,4 +17,30 @@ async function sendSite() {
     }
 }
 
+async function toGH(e) {
+    let check = document.getElementById("checkvalue");
+    if (check.checked) {
+        let body = "**The Issue**" + "%0A%0A";
+        body += document.getElementById("box2").value + "%0A%0A";
+        body += "**Computer Specifications**" + "%0A%0A";
+        body += document.getElementById("box2-2").value + "%0A%0A";
+        body += "**Extra Information**" + "%0A%0A";
+        body += document.getElementById("box3").value + "%0A%0A";
+        body += "_This issue was submitted via the Global Consent Manager Web Extension_";
+        let title = document.getElementById("box1").value;
+        console.log(title);
+        let [tab] = await browser.tabs.query({active: true, currentWindow: true});
+        await browser.tabs.create({
+            url: "https://github.com/SociallyCompute/Global-Consent-Manager/issues/new?title="
+            + document.getElementById("box1").value
+            + "&body="
+            + body
+            + "&projects=loading..." + tab.url + "&labels=not listed"});
+    } else {
+        console.log("DOESNT WORK");
+        document.getElementById("checkvalue2").style.color = "#cb4335";
+    }
+}
+
+document.getElementById("bugsubmit").addEventListener("click", toGH);
 window.addEventListener("DOMContentLoaded", sendSite);
